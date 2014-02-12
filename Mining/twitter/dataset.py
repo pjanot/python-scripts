b21683643aeccacfde4f6a947de1f7edfa7489e4
Colin <colin.bernet@cern.ch>
2014-02-12 13:25:17 +0100

    renamed datasample -> dataset. documented this class

--- Mining/twitter/dataset.py
+++ Mining/twitter/dataset.py
@@ -2,8 +2,23 @@ from collections import OrderedDict
 import pandas as pd
     
 class DataSet( OrderedDict ):
+    '''Class holding data before the creation of a dataframe. 
+
+    Example:
+    
+    import pandas as pd 
+    ds = DataSet(['var1', 'var2', 'var3'])
+    # ... fill the dataset ...
+    df = pd.DataFrame(ds)
+    '''
     
     def __init__(self, fields):
+        '''Create a dataset, with a list of fields.
+
+        Example: 
+        ds = DataSet(['var1', 'var2', 'var3'])
+
+        '''
         # not sure I want to declare the fields here.
         # might want to do it at class level
         # maybe there is no big deal. profile
@@ -12,29 +27,36 @@ class DataSet( OrderedDict ):
         for f in self._fields:
             self.setdefault( f, [])
 
-    def __iadd__(self, namedtuple):
-        if self._fields != namedtuple._fields:
+    def __iadd__(self, namtuple):
+        '''Add a row of values from a namedtuple, by doing: 
+        The fields must be compatible: namedtuple._fields == self._fields 
+
+        Example:
+        Row = namedtuple('Row', ['var1', 'var2', 'var3'])
+        row = Row(1.0, 2.0, 'a_label')
+        dataset += row
+
+        '''
+        if self._fields != namtuple._fields:
             errmsg = '''
-            trying to add namedtuple with fields:
+            trying to add namtuple with fields:
               {tuplef}
             to DataSet with fields
               {dsf} 
-            '''.format(tuplef=namedtuple._fields,                     
+            '''.format(tuplef=namtuple._fields,                     
                        dsf=self._fields)
             raise ValueError(errmsg)
         # can I do something more elegant? 
         for field in self._fields:
-            self[field].append( getattr(namedtuple, field) )           
+            self[field].append( getattr(namtuple, field) )           
         return self
     
-    def __str__(self):
-        thestr = self.repr()
-        return thestr
-
     def __repr__(self):
         repr = 'Dataset: {fields}'.format(fields=str(self._fields))
         return repr
-        
+
+
+    
 if __name__ == '__main__':
 
     import pprint 
@@ -45,13 +67,15 @@ if __name__ == '__main__':
     
     Row1 = namedtuple('Row1', ['var1', 'var2', 'var3'])
 
-    nentries = 1000000
+    nentries = 50
     for idx in range(nentries):
         if idx%1000 == 0:
             print idx
         v1 = 1 * np.random.randn() + 1
-        v2 = 2 * np.random.randn() + 2
-        v3 = 3 * np.random.randn() + 3     
+        v2 = idx%2
+        v3 = 'unit'
+        if idx%10 == 0: 
+            v3 = 'ten'
         row1 = Row1(v1, v2, v3)
         ds += row1
 
