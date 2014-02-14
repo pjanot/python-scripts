8026e1d35d88cba5aea9d6e11fefea7cc480ed11
Colin <colin.bernet@cern.ch>
2014-02-14 17:01:25 +0100

    added language selection to stream endpoint
    Signed-off-by: Colin <colin.bernet@cern.ch>

--- Mining/twitter/twitter_stream.py
+++ Mining/twitter/twitter_stream.py
@@ -11,13 +11,14 @@ print twitter_api
 
 twitter_stream = twitter.TwitterStream(auth=twitter_api.auth)
 
-def init_stream(query='', lang=None):   
+def init_stream(query='', outfile='tweets_stream.json', language='en'):   
     stream = None
     if query!='':
-        stream = twitter_stream.statuses.filter(track=query)
+        stream = twitter_stream.statuses.filter(track=query,
+                                                language=language)
     else: 
         # getting a small sample of all public tweets
-        stream = twitter_stream.statuses.sample()
+        stream = twitter_stream.statuses.sample(language=language)
 
     tweets = []
     ds = DataSet( Tweet._fields )
@@ -25,8 +26,6 @@ def init_stream(query='', lang=None):
         for tweet in stream:
             if tweet.get('delete'): 
                 # tweet deletions?
-                continue
-            if lang and tweet['lang']!=lang:
                 continue
             tweets.append( tweet )
             dtw = Tweet(tweet)
@@ -36,7 +35,7 @@ def init_stream(query='', lang=None):
     except KeyboardInterrupt:
         pass
 
-    with open('tweets_stream.json', 'w') as outfile:
+    with open(outfile, 'w') as outfile:
         json.dump(tweets, outfile)
         df = pd.DataFrame(ds)
     return tweets, df
@@ -44,4 +43,4 @@ def init_stream(query='', lang=None):
 if __name__ == '__main__':
 
     query = ','.join(sys.argv[1:])
-    tweets, df = init_stream( query, lang='en' )  
+    tweets, df = init_stream( query, language='fr' )  
