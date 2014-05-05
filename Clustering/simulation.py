bceb0dea174b4874e07838f16cb63ef781482003
Colin <colin.bernet@cern.ch>
2014-05-05 17:49:03 +0200

    attempt at starting to reorganize gaussian mixture code

--- Clustering/simulation.py
+++ Clustering/simulation.py
@@ -2,31 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
+
 from sklearn.mixture import GMM
 
-class Cluster(object):
+
+class GenCluster(object):
+    
     def __init__(self, mean, width, nsamples=100):
         self.mean = mean
         self.covar = [[width,0],[0,width]]
         self.samples = np.random.multivariate_normal(self.mean,
                                                      self.covar,
-                                                     nsamples)
-        
+                                                     nsamples)        
     def draw(self, color='b'):
         plt.scatter( self.samples[:,0], self.samples[:,1], c=color)
 
 
 
-class Simulator(object):
+class Gaussian(object):
 
-    def __init__(self, means):
-        self.simulate(means)
-        self.reconstruct()
-        self.draw()
-    
-    def simulate(self, means):
-        self.clusters = [Cluster(mean,0.2) for mean in means] 
-        self.samples = np.concatenate( [cluster.samples for cluster in self.clusters] )
+    def __init__(self, mean, sigma):
+        self.mean = mean
+        self.sigma = sigma
+
+    def draw(self, color='b'):
+        self.c1 = plt.Circle( self.mean, self.sigma,
+                              fc=color, ec=color, lw=2, alpha=0.2)
+        self.c2 = plt.Circle( self.mean, 2*self.sigma,
+                              fc=color, ec=color, lw=2, alpha=0.1)
+        plt.gcf().gca().add_artist(self.c1)
+        plt.gcf().gca().add_artist(self.c2)
+        
+    def __str__(self):
+        print 'gaussian', self.mean, self.sigma
+
+        
+
+class Event(object):
+
+    def __init__(self):
+        self.clusters = []
+        self.gmm = None
 
     def draw(self):
         self.fig = plt.figure(figsize=(10,10))
@@ -38,12 +54,9 @@ class Simulator(object):
             for icircle in range(self.gmm.n_components):
                 mean = self.gmm.means_[icircle]
                 covar = self.gmm.covars_[icircle]
-                sigma = sqrt(covar[0])
-                c1 = plt.Circle( mean, sigma, fc='b', ec='b', lw=2, alpha=0.2)
-                c2 = plt.Circle( mean, 2*sigma, fc='b', ec='b', lw=2, alpha=0.1)
-                self.fig.gca().add_artist(c1)
-                self.fig.gca().add_artist(c2)
-                
+                sigma = np.sqrt(covar[0])
+                g = Gaussian(mean, sigma)           
+                g.draw()
 
     def reconstruct(self, nclusters=None):
         if nclusters is None:
@@ -52,8 +65,25 @@ class Simulator(object):
                        covariance_type='spherical',
                        init_params='wc', n_iter=10)
         self.gmm.fit( self.samples )
+    
+        
+    
+class SimEvent(Event):
+
+    def __init__(self, means):
+        super(SimEvent, self).__init__()
+        self.simulate(means)
+        self.reconstruct()
+        self.draw()
+    
+    def simulate(self, means):
+        self.clusters = [GenCluster(mean,0.2) for mean in means] 
+        self.samples = np.concatenate( [cluster.samples for cluster in self.clusters] )
 
 
-simulator = Simulator(
-    [[-2,-2], [0,0], [2,1], [2,2]]
-    )
+
+
+
+simulator = SimEvent(
+     [[-2,-2], [0,0], [2,1], [2,2]]
+     )
