3c1185ffbf169b78e6319ae05a23ff9d2bf50dd8
Colin <colin.bernet@cern.ch>
2014-05-09 14:50:05 +0200

    revert linked list, recursive

--- Algorithms/revert_list.py
+++ Algorithms/revert_list.py
@@ -1,7 +1,7 @@
 
 from linkedlist import Node, build_list, print_list
 
-def revert(root):
+def revert_iterative(root):
     parent = None
     node = root
     child = root.child
@@ -18,11 +18,26 @@ def revert(root):
     return parent
 
 
+def revert_recursive(root, parent):
+    newroot = None
+    if root.child:
+        newroot = revert_recursive(root.child, root)
+    else:
+        newroot = root
+    root.child = parent
+    return newroot
+    
 if __name__ == '__main__':
 
     import pprint 
     root = build_list()
     print_list(root)
-
-    newroot = revert(root)
+    print
+    
+    newroot = revert_iterative(root)
     print_list(newroot)
+    print
+
+    newroot = revert_recursive(newroot, None)
+    print_list(newroot)
+    print
