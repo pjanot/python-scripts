e894a12c8940bb91689da59e17bc3c2246aab11a
Colin <colin.bernet@cern.ch>
2014-05-27 16:26:33 +0200

    added generator to iterate on a linked list

--- Algorithms/linkedlist.py
+++ Algorithms/linkedlist.py
@@ -1,11 +1,22 @@
 
 class Node(object):
+
     def __init__(self, value):
         self.child = None
         self.value = value
 
     def __repr__(self):
         return str(self.value)
+
+    def __iter__(self):
+        yield self.child
+
+
+def iterate(node):
+    while node is not None:
+        yield node
+        node = node.child
+
 
 def build_list(N=10):
     parent = None
@@ -19,14 +30,18 @@ def build_list(N=10):
         parent = node
     return root
 
+
 def print_list(root):
     while root:
         print root, '->', root.child
         root = root.child
-    
+
 
 if __name__ == '__main__':
 
-    import pprint 
+    import pprint
     root = build_list()
     print_list(root)
+
+    for node in iterate(root):
+        print node
